6536e75836081ca1e02764f960b7787c94bee8f7
jepio <jeremi.piotrowski@gmail.com>
2014-07-24 16:14:40 +0200

    Added header for libtest.

---
+++ libtest/libtest.h
@@ -0,0 +1,11 @@
+// API of the libtest.so shared library.
+
+// Generate a random array of integers.
+int *gen_array(int size);
+// Calculate the average of array arr of size ints.
+int average(int arr[], int size);
+// Test printf
+int main(void);
+
+
+
