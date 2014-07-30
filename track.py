8978e02b94558f1c33794a5baa0ef4cc21382d70
jepio <jeremi.piotrowski@gmail.com>
2014-07-30 18:21:14 +0200

    Added track module.

---
+++ track.py
@@ -0,0 +1,12 @@
+""" A module implementing various tracks. """
+# pylint: disable=R0903,C0103
+
+class Track(object):
+    """ A track interface. """
+
+    def __init__(self):
+        pass
+
+    def get_yintercept(self, x):
+        """ Return y intercept of track with detector. """
+        pass
