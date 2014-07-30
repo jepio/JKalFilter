86415bc1a795d6f1d13f1fb641a5888c44f4e4be
jepio <jeremi.piotrowski@gmail.com>
2014-07-30 18:53:05 +0200

    Added track module.

--- track.py
+++ track.py
@@ -1,7 +1,9 @@
 """ A module implementing various tracks. """
 # pylint: disable=R0903,C0103
 
+
 class Track(object):
+
     """ A track interface. """
 
     def __init__(self):
@@ -10,3 +12,16 @@ class Track(object):
     def get_yintercept(self, x):
         """ Return y intercept of track with detector. """
         pass
+
+
+class LineTrack(Track):
+
+    """ A straight line track with the equation :math:`y = a x + b`. """
+
+    def __init__(self, a, b):
+        super(LineTrack, self).__init__()
+        self.a = a
+        self.b = b
+
+    def get_yintercept(self, x):
+        return self.a * x + self.b
