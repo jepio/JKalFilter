481544a3e5245b82d25d705b0bb7951d6fab292d
jepio <jeremi.piotrowski@gmail.com>
2014-09-12 01:13:34 +0200

    Commented detector

--- detector.py
+++ detector.py
@@ -1,13 +1,27 @@
-""" Module implementing the detector and detector response system. """
+""" Module implementing detectors and the detectors interaction with :py:mod:`track`. """
 # pylint: disable=C0103,R0913,W0613,W0201,W0141
 import math
 from track import Track
-from matplotlib import pyplot as plt
 
 
 class Detector(object):
 
-    """ Abstract detector interface, to be inherited by all detectors. """
+    """Detector baseclass meant to be inherited by all detectors. Each
+    detector should implement 3 functionalities:
+
+    1. :py:meth:`clear_hits` to clear the hits stored in the detector
+    2. :py:meth:`hit` to hit the detector at the correct location.
+    3. :py:meth:`pos` to retries the position of the detector.
+
+    This baseclass provides default implementations of these 3 methods, but
+    they should be overridden for more complex detectors.
+
+    :param float x: horizontal position of detector
+    :param float y: vertical position of detector
+    :param float x_err: measurement(hit) horizontal uncertainty
+    :param float y_err: measurement(hit) vertical uncertainty
+    :var int hits: amount of hits registered in this detector
+    """
 
     def __init__(self, x, y, x_err=0, y_err=0):
         types = [int, float]
@@ -20,11 +34,16 @@ class Detector(object):
         self.hits = 0
 
     def hit(self, x, y):
-        """ Increment the amount of hits in this detector. """
+        """ Increment the amount of hits in this detector. The default
+        implementation increments the count by one, not checking passed
+        parameters"""
         self.hits += 1
 
     def pos(self):
-        """ Get the position of the detector. """
+        """ Get the position of the detector.
+
+        :returns: position of the detector (**x**, **y**)
+        :rtype: tuple(float)"""
         return (self.x, self.y)
 
     def clear_hits(self):
@@ -34,11 +53,23 @@ class Detector(object):
 
 class Strip(Detector):
 
-    """ A strip detector, part of a :py:class:`.Layer`. """
+    """ A strip detector, designed to be part of a :py:class:`.Layer`. If
+    *y_err* is ``None`` then the errors will be computed from uniform
+    distribution errors. The default method implementations in :py:class:`Detector`
+    are sufficient for this class.
 
-    def __init__(self, x, y, height, x_err=0, y_err=-1, parent=None):
+    :param float x: horizontal position of detector
+    :param float y: vertical position of detector
+    :param float height: The vertical size of the detector
+    :param float x_err: measurement(hit) horizontal uncertainty
+    :param float y_err: measurement(hit) vertical uncertainty
+    :param parent: a reference to the parent detector to which this strip
+     belongs.
+    """
+
+    def __init__(self, x, y, height, x_err=0, y_err=None, parent=None):
         # Uniform distribution errors
-        if y_err == -1:
+        if y_err == None:
             y_err = height / 12 ** 0.5
         super(Strip, self).__init__(x, y, x_err, y_err)
         self.height = height
@@ -47,7 +78,22 @@ class Strip(Detector):
 
 class Layer(Detector):
 
-    """ A layer detector containing :py:class:`.Strip`. """
+    """ A layer detector containing many :py:class:`.Strip` detectors, meant to
+    be part of a :py:class:`.LayeredDetector`. The :py:meth:`.clear_hits`
+    implementation in this class removes hits from this detector and all
+    detectors that belong to it. The tuple returned by :py:meth:`.pos` will hold
+    the *x* position of the layer and the *y* position of the middle.
+
+    Constructed strips will have *y* positions from ``y - height`` to
+    ``y + height``.
+
+    :param float x: horizontal position of detector
+    :param float y: horizontal position of detector
+    :param float height: the height of the whole layer
+    :param int num_strips: the amount of strips of which the layer consists -
+     they will be automatically constructed
+    :param parent: a reference to the parent detector to which this strip
+     belongs."""
 
     def __init__(self, x, y, height, num_strips, parent):
         super(Layer, self).__init__(x, y)
@@ -69,7 +115,13 @@ class Layer(Detector):
         self.parent = parent
 
     def hit(self, x, y):
-        """ Increment the hit amount in the correct strip. """
+        """ Increment the hit count in the correct strip. Will find the correct
+        strip based on the passed *y* parameter. The *x* parameter should
+        always be the same one as returned by ``self.pos()``.
+
+        :param float x: **x** position of this detector
+        :param float y: **y** position at which the detector was hit
+        """
         if self.x != x:
             raise RuntimeError("Wrong x of layer")
         # Needs to be this way for the rare case where y == self.top, because
@@ -95,7 +147,22 @@ class Layer(Detector):
 
 class LayeredDetector(Detector):
 
-    """ A layered detector, consisting of :py:class:`.Layer`. """
+    """ A layered detector, consisting of many :py:class:`.Layer` detectors.
+    Layers will be constructed based on the construction parameters. The
+    :py:meth:`.pos` method will return the *y* position of the middle of all
+    detectors and the *x* position of the leftmost layer. Layers will be
+    constrcuted between ``x`` and ``x + length``. The *num_layers* parameter
+    will be passed to the constructor of the :py:class:`Layer` objects.
+
+    :param float x: horizontal position of detector
+    :param float y: horizontal position of detector
+    :param float height: the height of each layer
+    :param float length: the length of the whole detector
+    :param int num_layers: the number of layers that will be constructed -
+     first one ``x``, last one at ``x + length``.
+    :param int num_strips: the number of strips that will be constructed in
+     **each** layer.
+    """
 
     def __init__(self, x, y, height, length, num_layers, num_strips):
         super(LayeredDetector, self).__init__(x, y)
@@ -123,7 +190,10 @@ class LayeredDetector(Detector):
 
 
     def get_layers(self, reverse=False):
-        """Generate the layers in the detector."""
+        """Helper generator that yields the layers in the detector.
+
+        :param bool reverse: toggle whether to yield in rising or falling *x*
+         order """
         # layers are sorted by x position
         layers = sorted(self.layers, key=lambda i: i.pos()[0])
         if reverse:
@@ -132,7 +202,12 @@ class LayeredDetector(Detector):
             yield layer
 
     def draw(self, rounding=False):
-        """ Draw the detector with points in each strip center. """
+        """ Draw the detector with blue markers in the center of each strip of
+        the detector and red markers at every registered hit. Pulls in
+        `matplotlib <http://matplotlib.org>`_ as a dependency. Additionally
+        prints the positions of all hits. If *rounding* is True rounds the
+        printed floating point values to 5 decimal digits. """
+        from matplotlib import pyplot as plt
         x = []
         y = []
         hits_x = []
@@ -168,7 +243,13 @@ class LayeredDetector(Detector):
         self.hits = 0
 
     def propagate_track(self, track):
-        """Propagate track through detector, leaving hits in strips."""
+        """Propagate track through detector, leaving hits in strips. The
+        passed track must extend the :py:class:`track.Track` baseclass. The
+        propagated track will leave hits in the strips of this detector.
+
+        :param track: track to propagate
+        :type track: Track
+        """
         assert isinstance(track, Track)
         # Sort layers according to x position (just in case)
         for layer in self.get_layers():
@@ -177,7 +258,11 @@ class LayeredDetector(Detector):
             layer.hit(x, y)
 
     def propagate_tracks(self, tracks):
-        """Propagate all tracks from a list, leaving hits in the detector."""
+        """Propagate all tracks from a list, leaving hits in the detector.
+
+        :param tracks: tracks to be propagated through the detector
+        :type tracks: list(Track)
+        """
         assert isinstance(tracks, list)
         for track in tracks:
             self.propagate_track(track)
